5ec307deaa755201054c04533c7fa06db660e1b5
Warlune <Warlune1@gmail.com>
2014-10-08 16:10:21 -0500

    added functions

--- to_do.php
+++ to_do.php
@@ -1,32 +1,54 @@
 <?php
 
+echo exec('clear');
 // Create array to hold list of todo items
 $items = [];
 array_unshift($items, "phoney");
 unset($items[0]);
+
+ // List array items formatted for CLI
+ function listitems($list) {
+    $string = '';
+    foreach($list as $key => $items) {
+        $key++;
+        $string .= "[{$key}] {$items}" . PHP_EOL;
+    }
+
+    return $string;
+}
+ // Get STDIN, strip whitespace and newlines,
+ // and convert to uppercase if $upper is true
+ function getinput($upper = false) {   
+
+    $input = strtoupper(trim(fgets(STDIN)));
+        return $input;
+    // Return filtered STDIN inpu
+}
+
 // The loop!
 do {
     // Iterate through list items
-    foreach ($items as $key => $item) {
-        // *** or $key++; ***
-        // Display each item and a newline
-        echo "[{$key}] {$item}\n";
-    }
-
+    // $input = '';
+    // foreach ($items as $key => $item) {
+    //     // *** or $key++; ***
+    //     // Display each item and a newline
+    //     echo "[{$key}] {$item}\n";
+    // }
+    echo $string = listitems($items);
     // Show the menu options
     echo '(N)ew item, (R)emove item, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
-    $input = strtolower(trim(fgets(STDIN)));
+      $input = getinput($upper);
 
     // Check for actionable input
-    if ($input === 'n')  {
+    if ($input === 'N')  {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
         $items[] = trim(fgets(STDIN));
-    } elseif ($input === 'r') {
+    } elseif ($input === 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
@@ -36,7 +58,7 @@ do {
         unset($items[$key]);
     }
 // Exit when input is (Q)uit
-} while ($input != 'q');
+} while ($input != 'Q');
 
 // Say Goodbye!
 echo 'Goodbye!' . PHP_EOL;
