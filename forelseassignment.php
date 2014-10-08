456b00dbb5b7c3ae37017428e3cc4f95c61a6c57
Warlune <Warlune1@gmail.com>
2014-10-08 09:33:22 -0500

    added thomas answer for tree assignment

--- forelseassignment.php
+++ forelseassignment.php
@@ -27,25 +27,52 @@ $trees = array(
 
 echo exec('clear');
 echo PHP_EOL;
+//-------------------------------------------------------------------
+foreach ($trees as $treeName => $properties) {
+ 
+    //echo "$treeName Tree ({$properties['genus']} {$properties['species']})" . PHP_EOL;
+    
+    // Alternative syntax for echo -- same output
+    echo $treeName . ' Tree' . ' (' . $properties['genus'] . ' ' . $properties['species'] . ') ' . PHP_EOL;
+ 
+        //Continue if tree's name is 'Scale'
+        //if ($treeName == 'Scale') {
+        //    continue;
+        //}
+ 
+        if ($properties['extinct'] == TRUE) {
+            echo 'Extinct?' . ' Yes' . PHP_EOL;
+ 
+            // Break if the tree is extinct
+            //break;
+        } 
+ 
+        else {
+            echo 'Extinct?' . ' No' . PHP_EOL;
+        }
+ 
+    echo '---------------' . PHP_EOL;
+}
+ 
+//-------------------------------------------------------------------
 
-
-foreach ($trees as $title => $properties) {
-    echo "$title Tree ";
-    echo "(" . $properties['genus'] . ' ';
-    echo $properties['species'] . ")" . PHP_EOL;
+// foreach ($trees as $title => $properties) {
+//     echo "$title Tree ";
+//     echo "(" . $properties['genus'] . ' ';
+//     echo $properties['species'] . ")" . PHP_EOL;
     
         
-        foreach ($properties as $thing => $thin) {
-         if (is_string($thin) == true) {
-         } elseif ((is_bool($thin) == true) && ($thin == true)) {
-             echo "Extinct? Yes\n";
-             echo "-------------------------\n";
-         } elseif ((is_bool($thin) == true) && ($thin == false)) {
-             echo "Extinct? No\n";
-             echo "-------------------------\n";
-         }
-     }
- }
+//         foreach ($properties as $thing => $thin) {
+//          if (is_string($thin) == true) {
+//          } elseif ((is_bool($thin) == true) && ($thin == true)) {
+//              echo "Extinct? Yes\n";
+//              echo "-------------------------\n";
+//          } elseif ((is_bool($thin) == true) && ($thin == false)) {
+//              echo "Extinct? No\n";
+//              echo "-------------------------\n";
+//          }
+//      }
+//  }
 
  
 // Achieve the following output:
